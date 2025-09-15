fbb8d73d7b0304ebf9c713f091b3854183a5c25d
BellaAkins <sarabellaakins@gmail.com>
2025-09-15 17:54:05 +0100

    start bellaverse repo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <section className="">
+      <div className="">
+        <h1 className="text-4xl text-amber-300 font-bold">Navbar</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
