50854eafe2a84bcf9e668f9ef294c330c6f07e39
BellaAkins <sarabellaakins@gmail.com>
2025-09-18 15:29:15 +0100

    made the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { HiX, HiMenu } from "react-icons/hi";
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -12,14 +13,24 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-yellow-400 backdrop-blur-sm border-b border-gray-100">
+    <nav className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm border-b border-gray-100">
       {/*For the Links*/}
       <div className="w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16 ">
         {/*logo*/}
         <div className="flex items-center gap-1 cursor-pointer">
-          <h2 className="">
-            Bella<span>Verse</span>
+          <h2 className="text-2xl">
+            Bella<span className="text-4xl text-pink-600">V</span>erse
           </h2>
+        </div>
+
+        {/*Mobile menu*/}
+        <div className="">
+          <button
+            onClick={() => setMenuOpen(!isMenuOpen)}
+            className="md:hidden p-2 "
+          >
+            {isMenuOpen ? <HiX className="size-6" /> : <HiMenu />}
+          </button>
         </div>
 
         {/*nav links*/}
@@ -45,6 +56,27 @@ const Navbar = () => {
           <a href="#">Let's Talk</a>
         </button>
       </div>
+
+      {/*For mobile Menu*/}
+      {isMenuOpen && (
+        <div className="md:hidden bg-white border-t border-gray-100 py-4">
+          <div className="container mx-auto px-4 space-y-3 ">
+            {navLinks.map((navLink, index) => (
+              <a
+                className={`block text-sm font-medium py-2 ${
+                  activeLink === navLink.href
+                    ? "text-pink-600"
+                    : "text-gray-600 hover:text-gray-900"
+                }  `}
+                key={index}
+                href={navLink.href}
+              >
+                {navLink.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
