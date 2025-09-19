6def5c7f4afc77146f1fc376735adc93cf00ee42
BellaAkins <sarabellaakins@gmail.com>
2025-09-19 22:09:50 +0100

    Added Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="container mx-auto pt-44 pb-6">
+      <h1 className="">Hero</h1>
+    </section>
+  );
+};
+
+export default Hero;
