c2e360587832049867ccecea76ab0323ce89f377
BellaAkins <sarabellaakins@gmail.com>
2025-09-25 18:59:54 +0100

    finish left column

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,43 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="container mx-auto pt-44 pb-6">
-      <h1 className="">Hero</h1>
+    <section className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-34 pb-6 px-4 sm:px-6 lg:px-6">
+      {/*Left column*/}
+      <div className="w-full md:w-1/2 space-y-8">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl mb-6 font-bold leading-tight">
+          Life dey happen
+          <span className="text-pink-600 inline-block">
+            grow, glow, and catch vibes
+          </span>{" "}
+          steady
+          <span className="inline-block ml-2 animate-pulse">🌸💡</span>
+        </h1>
+        <p className="text-gray-600 text-lg md:text-xl max-w-xl ">
+          Because growth isn’t boring—it’s messy, exciting, and full of
+          surprises. Here, you’ll find stories, lessons, and ideas that help you
+          learn, explore, and glow up
+        </p>
+        {/*input email*/}
+        <div className="flex gap-3">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email address"
+            className="  px-6 py-4 border border-pink-300 rounded-xl focus:outline-none focus:border-pink-600 
+           transition-all"
+          />
+          <button
+            className="bg-pink-600 text-white px-8 py-4 rounded-2xl hover:bg-pink-700 cursor-pointer
+          transition-all hover:shadow-lg hover:shadow-pink-300 "
+          >
+            →
+          </button>
+        </div>
+      </div>
+
+      {/*Right column*/}
+      <div className="w-full md:w-1/2"></div>
     </section>
   );
 };
